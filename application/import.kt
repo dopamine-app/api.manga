a736707840ddf9d96bbb044bfe1807a5bfb3cfb8
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-15 08:17:56 +0600

    Update import.kt

--- application/import.kt
+++ application/import.kt
@@ -1,1 +1,20 @@
+var AdmZip = require('adm-zip');
+const { join } = require('path');
 
+function bootstrap() {
+  const zip = new AdmZip();
+  zip.addLocalFolder(join('.', 'assets'), 'assets');
+  zip.addLocalFolder(join('.', 'images'), 'images');
+  zip.addLocalFolder(join('.', 'scripts'), 'scripts');
+  zip.addLocalFolder(join('.', 'styles'), 'styles', (name) =>
+    name.includes('.css'),
+  );
+  zip.addLocalFile(join('.', 'manifest.json'));
+  zip.addLocalFile(join('.', 'chapter.html'));
+  zip.addLocalFile(join('.', 'hui.html'));
+  zip.writeZip(join('.', 'webstore.zip'), () => {
+    console.log('Zip build complete');
+  });
+}
+
+bootstrap();
