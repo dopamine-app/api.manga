01be283f7300096850371f7fce3486c00638d475
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-15 16:24:25 +0600

    Create v1.kt

---
+++ v2/server/poster/v1.kt
@@ -0,0 +1,4 @@
+[ {
+"poster":"https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/201651620/original/e4a8a79fccae98487a74d8bd1f2357834dfa7295/design-a-high-quality-anime-twitter-banner.jpg",
+"id":"test"
+} ]
