427c01a14470c72e98aa6f9a20ea982304b1568e
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-18 14:18:43 +0600

    Update b1.kt

--- v2/server/b/b1.kt
+++ v2/server/b/b1.kt
@@ -1,4 +1,5 @@
 [ {
 "poster":"https://fast-anime.ru/storage/uploads/products/27678/2021/08/04/pyBOmchAcELtxkP0QjhY2ARpchaslzgJxCW2hUOx.jpeg",
-"title":"Соңғы Серафим"
+"title":"Соңғы Серафим",
+"id":"test"
 } ]
