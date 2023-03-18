45bb709034ca1385080efdd8f353af807df110b8
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-18 18:20:31 +0600

    Update test.kt

--- v2/server/tom/select/test.kt
+++ v2/server/tom/select/test.kt
@@ -1,5 +1,6 @@
 [ {
 "avatars":"https://yt3.googleusercontent.com/GKvzT2Xh4xpZ7hszNcSPAAuvdJYJgYeUBGik0cjmR2JnnXHaO3smdKvImDqixd-xxQQjF22aynY=s900-c-k-c0x00ffffff-no-rj",
 "chapter":"Dopamine Studio",
-"git":"Барлығы 439 глава"
+"git":"Барлығы 439 глава",
+"id":"test"
 } ]
