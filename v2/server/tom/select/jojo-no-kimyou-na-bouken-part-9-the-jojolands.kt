e6f5e2d4ac816411fef321d55bbf63727edf232b
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-19 15:58:30 +0600

    Create jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt

---
+++ v2/server/tom/select/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
@@ -0,0 +1,6 @@
+[ {
+"avatars":"https://yt3.googleusercontent.com/GKvzT2Xh4xpZ7hszNcSPAAuvdJYJgYeUBGik0cjmR2JnnXHaO3smdKvImDqixd-xxQQjF22aynY=s900-c-k-c0x00ffffff-no-rj",
+"chapter":"Ақ Қолшатыр",
+"git":"Барлығы 1 тарау • Қазақ тілінде",
+"id":"jojo-no-kimyou-na-bouken-part-9-the-jojolands"
+} ]
