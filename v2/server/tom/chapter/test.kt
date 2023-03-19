264dca1318ab5380633329fadb2e418394ecb1b1
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-19 20:36:07 +0600

    Update test.kt

--- v2/server/tom/chapter/test.kt
+++ v2/server/tom/chapter/test.kt
@@ -1,7 +1,9 @@
 [ {
 "chapter":"1 Том. 1-Тарау",
-"git":"19.03.2023"
+"git":"19.03.2023",
+"id":"https://m.vk.com/@bb_manga-nepobedimyi-proekt-ildzhin-54-glava"
 },
 {"chapter":"1 Том. 2-Тарау",
-"git":"19.03.2023"
+"git":"19.03.2023",
+"id":"https://m.vk.com/@bb_manga-nepobedimyi-proekt-ildzhin-54-glava"
 } ]
