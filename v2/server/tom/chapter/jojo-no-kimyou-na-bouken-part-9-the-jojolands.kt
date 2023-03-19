71f74a7c0bb3a3726c80d32b469eb044691869a7
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-19 19:40:51 +0600

    Update jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt

--- v2/server/tom/chapter/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
+++ v2/server/tom/chapter/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
@@ -1,5 +1,5 @@
 [ {
 "chapter":"1-Том. 1-Тарау",
 "git":"19.03.2023 • Ақ Қолшатыр",
-"id":"jojo-no-kimyou-na-bouken-part-9-the-jojolands"
+"id":"https://m.vk.com/@dop_source-dzhodzhony-shytyrman-hikayasy-dzhodzho-zherler-tom1-glava1"
 } ]
