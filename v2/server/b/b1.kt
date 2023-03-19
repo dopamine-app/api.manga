7300a5703cbb046cac76eb1cb7ac331bef0ce5de
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-19 15:50:04 +0600

    Update b1.kt

--- v2/server/b/b1.kt
+++ v2/server/b/b1.kt
@@ -2,4 +2,8 @@
 "poster":"https://fast-anime.ru/storage/uploads/products/27678/2021/08/04/pyBOmchAcELtxkP0QjhY2ARpchaslzgJxCW2hUOx.jpeg",
 "title":"Соңғы Серафим",
 "id":"test"
+},
+{"poster":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpu43wR7V3yzkpY51KqYB2Q9zB8_rwByppLQ&usqp=CAU",
+"title":"ДжоДжоның Шытырман Хикаясы: ДжоДжо жерлері",
+"id":"jojo-no-kimyou-na-bouken-part-9-the-jojolands"
 } ]
