a9c03295509df8178533d8093a0fafcbdd8f6220
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-19 15:46:28 +0600

    Update jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt

--- v2/server/release-page/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
+++ v2/server/release-page/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
@@ -1,2 +1,19 @@
 [ {
 "poster":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpu43wR7V3yzkpY51KqYB2Q9zB8_rwByppLQ&usqp=CAU",
+"title":"ДжоДжоның Шытырман Хикаясы: ДжоДжо жерлері",
+"title_original":"JoJo no Kimyou na Bouken Part 9: The JoJoLands",
+"age":"16+",
+"release_year":"2023 ж.",
+"style":"Маңга",
+"star":"9.80",
+"status":"Онгоинг",
+"translate_status":"Жалғасуда",
+"chapter":"1",
+"play":"null",
+"format":"Қара/Ақ түсті",
+"author":"ARAKI Hirohiko",
+"artist":"ARAKI Hirohiko",
+"publisher":"Shueisha",
+"description":"",
+"id":"jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt"
+} ]
