726d60552c5bf7a89c58b373aaf07ce8fe74f49a
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-27 11:52:55 +0600

    Update jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt

--- v2/server/tom/chapter/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
+++ v2/server/tom/chapter/jojo-no-kimyou-na-bouken-part-9-the-jojolands.kt
@@ -1,5 +1,5 @@
 [ {
 "chapter":"1-Том. 1-Тарау",
 "git":"19.03.2023 • Ақ Қолшатыр",
-"id":"https://psv4.userapi.com/c235131/u547207490/docs/d34/c71af1527cb5/JauaptyQalau.pdf?extra=6e_twnFTrFuTi70NHGTHrK5lxpD3NQES24-iOXWByfslbwsG5nBZqP_3NT7qKxilkOjaibXkywdD0kbqQBIAeGeSuDNVpHqVnM93NyUWYwdJ-N0BijZjriogHK8TXiLYbICKPtdrMXg8mslnwY1hdPOZMwU"
+"id":"https://psv4.userapi.com/c235131/u547207490/docs/d34/c71af1527cb5/JauaptyQalau.pdf"
 } ]
